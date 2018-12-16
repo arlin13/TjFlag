d90ec971d500af8a287d8aff6134da6b4483c192
Arlin Grijalba <arlin.grijalba@gmail.com>
2018-12-16 11:39:18 -0800

    Create header and added functionality to show correct UI according to user state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   renderContent() {
+    console.log("Im inside of class Header -> renderContent");
+    console.log("Value of props");
+    console.log(this.props);
     switch (this.props.auth) {
       case null:
         return;
@@ -12,12 +16,14 @@ class Header extends Component {
             <a href="/auth/google">Log in with Google</a>
           </li>
         );
-      default:
+      case true:
         return (
           <li>
             <a href="/api/logout">Logout</a>
           </li>
         );
+      default:
+        return <li> Not sure what is happening</li>;
     }
   }
   render() {
@@ -25,9 +31,13 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo" href="#">
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+            href=""
+          >
             Tj Flag
-          </a>
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
@@ -38,5 +48,9 @@ class Header extends Component {
 function mapStateToProps({ auth }) {
   return { auth };
 }
+// ???
+// const mapStateToProps = ({auth}) => {
+//   return { auth };
+// };
 
 export default connect(mapStateToProps)(Header);
--- client/src/index.js
+++ client/src/index.js
@@ -12,8 +12,7 @@ const store = createStore(() => reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    {" "}
-    <App />{" "}
+    <App />
   </Provider>,
   document.querySelector("#root")
 );
