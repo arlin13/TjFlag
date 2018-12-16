e4dfa00f44114c2223c890568b1f2b7ce70ffd54
Arlin Grijalba <arlin.grijalba@gmail.com>
2018-12-16 00:32:22 -0800

    Header showing correct UI according to user state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,42 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Log in with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
+    console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Tj Flag</a>
-          <ul className="right">
-            <li>
-              {" "}
-              <a> Login with Google</a>
-            </li>
-          </ul>
+          <a className="left brand-logo" href="#">
+            Tj Flag
+          </a>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
