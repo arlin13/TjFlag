65633468de387311b5d34c0738cb64d4f672427e
Arlin Grijalba <arlin.grijalba@gmail.com>
2018-12-19 18:05:30 -0800

    Changed listen function

--- index.js
+++ index.js
@@ -40,4 +40,10 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, function() {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
