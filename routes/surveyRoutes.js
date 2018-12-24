bbac71b938832d8991442bab03e0f54e0e80bfb7
Arlin Grijalba <arlin.grijalba@gmail.com>
2018-12-24 10:26:03 -0800

    Fixed typos in surveyRoutes (when importing Mailer)

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
-const mailer = require("../services/Mailer");
+const Mailer = require("../services/Mailer");
 const surveyTemplate = require("../services/templates/surveyTemplate");
+
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.post("api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
       title,
-      body,
       subject,
+      body,
       recipients: recipients.split(",").map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
