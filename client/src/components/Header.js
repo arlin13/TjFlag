c6fb91cc1ad5e895676f4ccfe530f6c06bd65bce
Arlin Grijalba <arlin.grijalba@gmail.com>
2019-01-01 17:07:57 -0800

    Fixed navbar to shrink according to device size

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 // import Payments from "./Payments";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css";
 
 class Header extends Component {
   renderContent() {
@@ -53,11 +55,17 @@ class Header extends Component {
         <div className="nav-wrapper">
           <Link
             to={this.props.auth ? "/dashboard" : "/"}
-            className="left brand-logo"
+            className="brand-logo"
           >
             Tj Flag
           </Link>
-          <ul className="right">{this.renderContent()}</ul>
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
+          <ul className="right hide-on-med-and-down">{this.renderContent()}</ul>
+          <ul className="sidenav" id="mobile-demo">
+            {this.renderContent()}
+          </ul>
         </div>
       </nav>
     );
@@ -67,5 +75,14 @@ class Header extends Component {
 function mapStateToProps({ auth }) {
   return { auth };
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, {
+    inDuration: 350,
+    outDuration: 350,
+    edge: "left"
+  });
+});
 
 export default connect(mapStateToProps)(Header);
